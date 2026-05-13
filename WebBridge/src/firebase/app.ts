40fccb0ceaffeb03f49df2136ef93ae24807d9ca
Mari <72758580+Mariana4Alves@users.noreply.github.com>
2026-05-13 01:14:05 -0300

    Update Firebase configuration with new API key and app ID

--- WebBridge/src/firebase/app.ts
+++ WebBridge/src/firebase/app.ts
@@ -1,11 +1,10 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDElepKQDGyA4",
+  apiKey: "AIzaSyDElepKQDGyA4-kHcLGNUrL8a2B9Buvdzo",
   authDomain: "battleship-multiplayer-cc51c.firebaseapp.com",
   projectId: "battleship-multiplayer-cc51c",
-  // TODO: obter configuração
-  appId: "..",
+  appId: "1:532294141124:web:484bc317784f6f9cabb591",
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
